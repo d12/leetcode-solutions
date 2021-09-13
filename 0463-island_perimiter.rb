cec99664102bd3e551272d5a059843677b97500e
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2021-09-13 10:44:22 -0400

    Add BFS solution to Island Perimiter

--- 0463-island_perimiter.rb
+++ 0463-island_perimiter.rb
@@ -13,4 +13,68 @@ def island_perimeter(grid)
   end
 end
 
-# TODO: Try this again with BFS/DFS. Can there be "islands"? if not, these will work fine.
+
+# BFS solution
+# O(N) time, O(N) space
+def island_perimiter(grid)
+  # First, find a land square
+
+  tile_index = nil
+  grid.each_with_index do |outer, i|
+    outer.each_with_index do |tile, j|
+      if tile == 1
+        tile_index = [i, j]
+        break
+      end
+    end
+
+    break if tile_index
+  end
+
+  queue = [tile_index]
+  visited = {}
+  sum = 0
+
+  until queue.empty?
+    current_x, current_y = queue.shift
+    if visited[[current_x, current_y]]
+      next
+    else
+      visited[[current_x, current_y]] = true
+    end
+
+    if grid[current_x][current_y] == 0
+      next
+    end
+
+    # left
+    if current_x == 0 || grid.dig(current_x - 1, current_y) == 0
+      sum += 1
+    else
+      queue << [current_x - 1, current_y]
+    end
+
+    # down
+    if current_y == 0 || grid.dig(current_x, current_y - 1) == 0
+      sum += 1
+    else
+      queue << [current_x, current_y - 1]
+    end
+
+    # right
+    if grid.dig(current_x + 1, current_y) == 0
+      sum += 1
+    else
+      queue << [current_x + 1, current_y]
+    end
+
+    #up
+    if grid.dig(current_x, current_y + 1) == 0
+      sum += 1
+    else
+      queue << [current_x, current_y + 1]
+    end
+  end
+
+  sum
+end
