23659be80efa304bc6f876afe679007b4df1e098
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2021-09-15 16:21:25 -0400

    Add tabluating DP solution to Fib

--- 0509-fibbonacci-number.rb
+++ 0509-fibbonacci-number.rb
@@ -1,12 +1,33 @@
 # Recursive solution
 # Using a memo object to cache progress
-# O(N) time, O(1) space
+# O(N) time, O(N) space for recursive call stacks
 def fib(n, memo = {})
   return 0 if n == 0
   return 1 if n <= 2
   return memo[n] if memo[n]
 
   fib(n-1, memo) + fib(n-2, memo)
+end
+
+# Tabulating DP solution
+# O(N) time, O(N) space
+def fib(n)
+  # Initialize table
+  table = Array.new(n + 1) { 0 }
+
+  # Seed table
+  table[0] = 0
+  table[1] = 1
+
+  # Populate table
+
+  table.each_with_index do |_val, i|
+    next if i == 0 || i == 1 # Already seeded
+
+    table[i] = table[i - 1] + table[i - 2]
+  end
+
+  table[n]
 end
 
 # Iterative solution, sidesteps limits with recursion depth and stack space
