86dd4865d30c753030c82ce0944f215bed742df7
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2021-09-15 16:15:14 -0400

    Add DP solution to Counting Bits

--- 0338-counting_bits.rb
+++ 0338-counting_bits.rb
@@ -1,4 +1,4 @@
-# O(N) time, O(1) space
+# O(N) time, O(N) space for solution
 def count_bits(n)
   (n+1).times.map do |i|
     one_bits_in_num(i)
@@ -24,4 +24,29 @@ def one_bits_in_num(num)
   17.times.count do |n|
       num & (2**n) != 0
   end
+end
+
+# DP solution
+# We know "1" is 1 in binary, and "0" is 0 in binary
+# Build off that
+# If we haven't seen a specific number, divide by 2 and recursively check that one.
+# if a number is odd, add one to the result of the prev number
+# Do this for each number. Memoize each result in a memo table
+#
+# Time: O(N)
+# Space: O(N)
+def count_bits(num)
+  table = Array.new(num + 1) { nil }
+  table[0] = 0
+  table[1] = 1 if num > 0
+
+  table.each_with_index do |num, i|
+    next if num
+    table[i] = table[i / 2]
+    if i % 2 == 1
+      table[i] += 1
+    end
+  end
+
+  table
 end
