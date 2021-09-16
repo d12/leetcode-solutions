4591cbf183fd49eff438db312fc937dbee25ab01
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2021-09-16 14:03:04 -0400

    Add more solutions for 746

--- 0746-min_cost_climbing_stairs.rb
+++ 0746-min_cost_climbing_stairs.rb
@@ -1,4 +1,4 @@
-# O(N) time, O(n^2) space
+# O(N) time, O(n) space since costs is passed as a reference and not modified
 def min_cost_climbing_stairs(cost)
   min_cost_climbing_stairs_recursive([0] + cost, 0, {})
 end
@@ -13,4 +13,39 @@ def min_cost_climbing_stairs_recursive(costs, index, memo)
   ].min
 
   memo[index] = min + costs[index]
+end
+
+# Tabulation
+# Min cost for a stair is the min for either of the last 2, plus itself
+# O(N) time, O(N) space (table)
+def min_cost_climbing_stairs(cost)
+  cost = cost + [0]
+  table = Array.new(cost.length) { nil }
+  table[0] = cost[0]
+  table[1] = cost[1]
+
+  table.each_with_index do |_val, i|
+    next if i < 2
+    table[i] = [table[i-1], table[i-2]].min
+    table[i] += cost[i]
+  end
+
+  table.last
+end
+
+# Tabulation with space optimization
+# O(N) time, O(1) space
+def min_cost_climbing_stairs(cost)
+  cost = cost + [0]
+
+  last = cost[1]
+  last_last = cost[0]
+
+  cost[2..-1].each do |cost|
+    new_cost = cost + [last, last_last].min
+    last_last = last
+    last = new_cost
+  end
+
+  last
 end
