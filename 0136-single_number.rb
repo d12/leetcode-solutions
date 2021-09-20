33897aa6038ab0963b1effe496f5f67091848a2b
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2021-09-20 15:59:27 -0400

    Single Number

--- 0136-single_number.rb
+++ 0136-single_number.rb
@@ -12,3 +12,13 @@ def single_number(nums)
 
   seen_once.first[0]
 end
+
+# O(n) time, O(1) space, bitwise solution
+def single_number(nums)
+  sum = 0
+  nums.each do |num|
+    sum ^= num
+  end
+
+  sum
+end
